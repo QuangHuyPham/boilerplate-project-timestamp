72473e84915bbcf6ab2844986fe1d61402bfdd93
Huy Pham <huypham.ictu@gmail.com>
2021-01-19 09:25:59 +0000

    first commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+var dayjs = require('dayjs')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -22,6 +23,21 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.get("/api/timestamp/:date?", async function (req, res) {
+  if (!req.params.date) {
+      // console.log( dayjs().valueOf(), `${dayjs().toString()}`)
+      return res.json({unix: dayjs().valueOf(), utc: dayjs().format()});
+  }
+
+  if(dayjs(req.params.date).isValid()) {
+    let time = dayjs(req.params.date).format();
+    if (+req.params.date > 0) time = dayjs(+req.params.date).format();
+    return res.json({unix: dayjs(time).valueOf(), utc: dayjs(time).toString()})
+  }
+
+  return res.json({ error : "Invalid Date" });
 });
 
 
